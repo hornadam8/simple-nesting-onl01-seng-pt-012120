667cf4e2a413f8cdd9dac8ff29efd6d3ec4e540e
Adam G Horn <hornadam8@gmail.com>
2020-01-28 04:35:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,8 +110,11 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C","Assembly"]
+          :languages => ["C"]
         }
      }
+    programmer_hash[:dennis_ritchie][:languages] += "Assembly"
+    
+    programmer_hash
 
 end
